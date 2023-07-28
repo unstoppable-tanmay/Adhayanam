2dee2803c098c7431a54e81c12c5d097b8079032
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2023-07-29 02:26:20 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/adhayanam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.adhayanam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/auth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => AuthScreenState();
+}
+
+class AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
